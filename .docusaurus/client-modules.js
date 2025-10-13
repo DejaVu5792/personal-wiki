c35d5851a0b4304a0a753f509daadf588e1cea38
DejaVu <dejavu5792@proton.me>
2025-10-13 19:17:46 +0800

    Deploy website - based on ae8c3205cf4711fcb503383d0a563ec4283f1292

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/dejavu/Documents/GitRepos/personal-wiki/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/dejavu/Documents/GitRepos/personal-wiki/node_modules/infima/dist/css/default/default.css"),
-  require("/home/dejavu/Documents/GitRepos/personal-wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/dejavu/Documents/GitRepos/personal-wiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/dejavu/Documents/GitRepos/personal-wiki/src/css/custom.css"),
-];
